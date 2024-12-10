2ace3c3095ea3711f4f943b14f1966739c1402e4
joaquinjakuu <162701432+joaquinjakuu@users.noreply.github.com>
2024-12-10 09:20:35 -0300

    uohuguibgiu

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,12 +6,14 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:mobile_scanner/src/web/mobile_scanner_web.dart';
 import 'package:permission_handler_html/permission_handler_html.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseCoreWeb.registerWith(registrar);
   MobileScannerWeb.registerWith(registrar);
   WebPermissionHandler.registerWith(registrar);
   registrar.registerMessageHandler();
